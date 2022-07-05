33c4d9c65b3f9488e1182d0486104d2929882451
Paulo Henrique <krobrelus@gmail.com>
2022-07-04 21:31:23 -0400

    commit 2

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
 import express from 'express';
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req,res) => {
+  return res.json({message: "hello word"})
+})
+
 app.listen(3333, () => console.log("Server Is Runing"));
